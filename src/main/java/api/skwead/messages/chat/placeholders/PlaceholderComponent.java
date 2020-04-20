dd90152438c10d597a75a01923e91c6c3cec7103
Skwead <skweadhunter.final@gmail.com>
2020-04-20 21:14:57 +0100

    Added PAPI support.

---
+++ src/main/java/api/skwead/messages/chat/placeholders/PlaceholderComponent.java
@@ -0,0 +1,25 @@
+package api.skwead.messages.chat.placeholders;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.function.BiFunction;
+
+public class PlaceholderComponent {
+
+    private String name;
+    private BiFunction<JavaPlugin, Player, String> getValue;
+
+    public PlaceholderComponent(String name, BiFunction<JavaPlugin, Player, String> getValue) {
+        this.name = name;
+        this.getValue = getValue;
+    }
+
+    public String getValue(JavaPlugin plugin, Player player){
+        return getValue.apply(plugin, player);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
